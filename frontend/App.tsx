2fc9bc8316bc4c1c6d8f2f4ec19e9799313af291
Vance Winstead <vance.winstead@gmail.com>
2022-01-09 18:22:28 -0800

    Linter hook test

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
 
