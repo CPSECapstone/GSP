db16db439e710af28efbae3d700534c3506b4938
NestorJMartinez <43479997+NestorJMartinez@users.noreply.github.com>
2022-02-22 13:34:24 -0800

    fixed linter errors

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -2,29 +2,6 @@ import React, { useState } from "react";
 import { SearchBar as SearchBarProp } from "react-native-elements";
 import { View, StyleSheet } from "react-native";
 
-type SearchBarComponentProps = {};
-
-const SearchBar: React.FunctionComponent<SearchBarComponentProps> = () => {
-  const [search, setSearch] = useState("");
-
-  const updateSearch = (search: React.SetStateAction<string>) => {
-    setSearch(search);
-  };
-
-  return (
-    <View style={styles.searchbar}>
-      <SearchBarProp
-        placeholder="Search"
-        containerStyle={styles.container}
-        inputContainerStyle={styles.input}
-        onChangeText={updateSearch}
-        value={search}
-        platform={"ios"}
-      />
-    </View>
-  );
-};
-
 const styles = StyleSheet.create({
   searchbar: {
     position: "absolute",
@@ -40,5 +17,26 @@ const styles = StyleSheet.create({
     backgroundColor: "#EBEBEB",
   },
 });
+
+function SearchBar() {
+  const [searchText, setSearchText] = useState("");
+
+  const updateSearch = (search: React.SetStateAction<string>) => {
+    setSearchText(search);
+  };
+
+  return (
+    <View style={styles.searchbar}>
+      <SearchBarProp
+        placeholder="Search"
+        containerStyle={styles.container}
+        inputContainerStyle={styles.input}
+        onChangeText={updateSearch}
+        value={searchText}
+        platform="ios"
+      />
+    </View>
+  );
+}
 
 export default SearchBar;
