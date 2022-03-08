d321539f9d751397f9ab3075b3cefb88cbd94b6f
Finlay <fpiroth@calpoly.edu>
2022-03-08 14:17:46 -0800

    linted

--- components/OwnershipTransfer/NotificationsPage.tsx
+++ components/OwnershipTransfer/NotificationsPage.tsx
@@ -4,30 +4,6 @@ import selectAllUserNotifs from "../../redux/selectors/notifications";
 import { NotificationsProps } from "../../route-settings";
 import OwnershipNotif from "./OwnershipNotif";
 
-const testData = [
-  {
-    title: "Ownership Request for @Bites",
-    message:
-      "Hello, this is Marvis , the owner of @Bites. Could you possibly transfer ownership of this to me so I can manage our profile? Thanks!",
-    senderID: "whatev",
-    type: "REQUEST",
-  },
-  {
-    title: "Ownership Request for @Bites",
-    message:
-      "Hello, this is Marvis , the owner of @Bites. Could you possibly transfer ownership of this to me so I can manage our profile? Thanks!",
-    senderID: "whatev",
-    type: "REJECTED",
-  },
-  {
-    title: "Ownership Request for @Bites",
-    message:
-      "Hello, this is Marvis , the owner of @Bites. Could you possibly transfer ownership of this to me so I can manage our profile? Thanks!",
-    senderID: "whatev",
-    type: "APPROVE",
-  },
-];
-
 // eslint-disable-next-line
 function Notifications({ route, navigation }: NotificationsProps) {
   const notifs = useAppSelector(selectAllUserNotifs);
@@ -54,18 +30,16 @@ function Notifications({ route, navigation }: NotificationsProps) {
                   type={item.type}
                 />
               );
-            } else {
-              return <Text>Invalid notification data.</Text>;
             }
-          } else {
-            return <Text>Error fetching notification data.</Text>;
+            return <Text>Invalid notification data.</Text>;
           }
+          return <Text>Error fetching notification data.</Text>;
         }}
         data={notifs}
         keyExtractor={(item, index) =>
           item?.Sender
             ? item.Sender
-            : "NO_SENDER" + item?.userID + index.toString()
+            : `NO_SENDER${item?.userID}${index.toString()}`
         }
       />
     </View>
