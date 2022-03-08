58508bc18acce88a286e39cbce41ae2bb9c6d2dc
Finlay <fpiroth@calpoly.edu>
2022-03-08 12:16:42 -0800

    pulling from master

--- src/APITypes.ts
+++ src/APITypes.ts
@@ -1,6 +1,6 @@
 import { DeepOmit } from './DeepOmit';
 import {
-  GetBusinessQuery, GetUserQuery, ListBusinessesQuery,
+  GetBusinessQuery, GetNotificationQuery, ListBusinessesQuery, ListNotificationsQuery,
 } from './API';
 
 export type BusinessQueryType = NonNullable<ListBusinessesQuery['listBusinesses']>['items'];
@@ -10,4 +10,17 @@ export type BusinessType = DeepOmit<
   '__typename'
 >;
 
+<<<<<<< Updated upstream
 export type UserType = GetUserQuery['getUser']
+=======
+>>>>>>> Stashed changes
+export type NotificationQueryType = NonNullable<ListNotificationsQuery['listNotifications']>['items'];
+
+export type NotificationType = DeepOmit<
+  Exclude<GetNotificationQuery['getNotification'], null | undefined>,
+  '__typename'
+<<<<<<< Updated upstream
+>
+=======
+>
+>>>>>>> Stashed changes
